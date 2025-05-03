a9efeeba34ae53f78e9852a75fc90ed207d11faa
vmeessen <vmeessen@DESKTOP-N6LS3D2.>
2025-05-03 14:07:51 +0200

    update

--- Minishell/include/minishell.h
+++ Minishell/include/minishell.h
@@ -1,12 +1,25 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <unistd.h> //	close(), read(), write()
-# include <stdlib.h> //	malloc(), free(), exit()
-# include <fcntl.h> //open()
-# include <sys/wait.h> //	wait(), waitpid()
-# include <stdio.h> //	perror()
-# include "../Lib/Libft/libft.h" //	ft_split(), ft_strjoin(), ft_strdup()..
+# include <unistd.h>	// close(), read(), write(), access(), fork(), getcwd(), chdir(), dup(), dup2(), pipe(), isatty(), unlink(), execve()
+# include <stdio.h>		// printf(), perror(), readline()
+# include <stdlib.h>	// malloc(), free(), exit(), getenv()
+# include <string.h>	// strerror()
+# include <fcntl.h>		// open()
+# include <sys/wait.h>	// wait(), waitpid(), wait3(), wait4()
+# include <signal.h>	// signal(), sigaction(), sigemptyset(), sigaddset(), kill()
+# include <sys/stat.h>	// stat(), lstat(), fstat()
+# include <dirent.h>	// Directory : opendir(), readdir(), closedir()
+# include <term.h>      // ttyname(), ttyslot()
+# include <sys/ioctl.h> // ioctl()
+# include <termios.h>	// tcsetattr(), tcgetattr()
+# include <curses.h>	// tgetent(), tgetflag(), tgetnum(), tgetstr(), tgoto(), tputs
+# include <readline/readline.h> // readline(), rl_clear_history(), rl_on_new_line(), rl_replace_line(), rl_redisplay(), add_history()
 
+// # include <errno.h>	// (used with perror(), strerror())
+// # include <term.h>	// (used with curses.h)
+
+
+# include "../Lib/Libft/libft.h" //	ft_split(), ft_strjoin(), ft_strdup()..
 
 #endif
--- Minishell/srcs/main.c
+++ Minishell/srcs/main.c
@@ -2,6 +2,18 @@
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	ft_putstr_fd("Hello World\n", 1);
+	// ft_putstr_fd("Hello Worlds\n", 1);
 
+	char *line = readline(">");
+
+	if (line == NULL)
+    {
+        printf("EOF detected. Exiting...\n");
+        return (0);
+    }
+
+	printf("line: %s\n", line);
+
+	free(line);
+	return (0);
 }
